a3f29cd8d7bfde7ab2fce9209a483fc51f741357
Babygogogo <101599838@qq.com>
2018-06-16 02:00:00 +0800

    [add] Update publish_copy.bat. Modify the destination for file copies.

--- publish_copy.bat
+++ publish_copy.bat
@@ -1,4 +1,4 @@
 
 call rd /s /q "bin-release/web/twc"
 call egret publish --version twc
-call xcopy "bin-release/web/twc" "../TinyWarsServer/build" /E /Y /I
+call xcopy "bin-release/web/twc" "../TinyWarsServer/build/client" /E /Y /I
