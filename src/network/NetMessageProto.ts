2b1c9b09d5653dd3d0463ea6f9873b35d2cac12c
Babygogogo <101599838@qq.com>
2018-06-20 00:45:08 +0800

    [update] Update proto definition.

--- src/network/NetMessageProto.ts
+++ src/network/NetMessageProto.ts
@@ -1,9 +1,8 @@
 
 namespace Network {
     export namespace Proto {
-
         /** Properties of a C_Register. */
-        export interface IC_Register {
+        export declare interface IC_Register {
 
             /** C_Register msgCode */
             msgCode?: (number | null);
@@ -15,8 +14,97 @@ namespace Network {
             password?: (string | null);
         }
 
+        /** Represents a C_Register. */
+        export declare class C_Register implements IC_Register {
+
+            /**
+             * Constructs a new C_Register.
+             * @param [properties] Properties to set
+             */
+            constructor(properties?: IC_Register);
+
+            /** C_Register msgCode. */
+            public msgCode: number;
+
+            /** C_Register account. */
+            public account: string;
+
+            /** C_Register password. */
+            public password: string;
+
+            /**
+             * Creates a new C_Register instance using the specified properties.
+             * @param [properties] Properties to set
+             * @returns C_Register instance
+             */
+            public static create(properties?: IC_Register): C_Register;
+
+            /**
+             * Encodes the specified C_Register message. Does not implicitly {@link C_Register.verify|verify} messages.
+             * @param message C_Register message or plain object to encode
+             * @param [writer] Writer to encode to
+             * @returns Writer
+             */
+            public static encode(message: IC_Register, writer?: protobuf.Writer): protobuf.Writer;
+
+            /**
+             * Encodes the specified C_Register message, length delimited. Does not implicitly {@link C_Register.verify|verify} messages.
+             * @param message C_Register message or plain object to encode
+             * @param [writer] Writer to encode to
+             * @returns Writer
+             */
+            public static encodeDelimited(message: IC_Register, writer?: protobuf.Writer): protobuf.Writer;
+
+            /**
+             * Decodes a C_Register message from the specified reader or buffer.
+             * @param reader Reader or buffer to decode from
+             * @param [length] Message length if known beforehand
+             * @returns C_Register
+             * @throws {Error} If the payload is not a reader or valid buffer
+             * @throws {protobuf.util.ProtocolError} If required fields are missing
+             */
+            public static decode(reader: (protobuf.Reader | Uint8Array), length?: number): C_Register;
+
+            /**
+             * Decodes a C_Register message from the specified reader or buffer, length delimited.
+             * @param reader Reader or buffer to decode from
+             * @returns C_Register
+             * @throws {Error} If the payload is not a reader or valid buffer
+             * @throws {protobuf.util.ProtocolError} If required fields are missing
+             */
+            public static decodeDelimited(reader: (protobuf.Reader | Uint8Array)): C_Register;
+
+            /**
+             * Verifies a C_Register message.
+             * @param message Plain object to verify
+             * @returns `null` if valid, otherwise the reason why it is not
+             */
+            public static verify(message: { [k: string]: any }): (string | null);
+
+            /**
+             * Creates a C_Register message from a plain object. Also converts values to their respective internal types.
+             * @param object Plain object
+             * @returns C_Register
+             */
+            public static fromObject(object: { [k: string]: any }): C_Register;
+
+            /**
+             * Creates a plain object from a C_Register message. Also converts values to other types if specified.
+             * @param message C_Register
+             * @param [options] Conversion options
+             * @returns Plain object
+             */
+            public static toObject(message: C_Register, options?: protobuf.IConversionOptions): { [k: string]: any };
+
+            /**
+             * Converts this C_Register to JSON.
+             * @returns JSON object
+             */
+            public toJSON(): { [k: string]: any };
+        }
+
         /** Properties of a S_Register. */
-        export interface IS_Register {
+        export declare interface IS_Register {
 
             /** S_Register msgCode */
             msgCode?: (number | null);
@@ -25,9 +113,96 @@ namespace Network {
             status?: (S_Register.Status | null);
         }
 
-        export namespace S_Register {
+        /** Represents a S_Register. */
+        export declare class S_Register implements IS_Register {
+
+            /**
+             * Constructs a new S_Register.
+             * @param [properties] Properties to set
+             */
+            constructor(properties?: IS_Register);
+
+            /** S_Register msgCode. */
+            public msgCode: number;
+
+            /** S_Register status. */
+            public status: S_Register.Status;
+
+            /**
+             * Creates a new S_Register instance using the specified properties.
+             * @param [properties] Properties to set
+             * @returns S_Register instance
+             */
+            public static create(properties?: IS_Register): S_Register;
+
+            /**
+             * Encodes the specified S_Register message. Does not implicitly {@link S_Register.verify|verify} messages.
+             * @param message S_Register message or plain object to encode
+             * @param [writer] Writer to encode to
+             * @returns Writer
+             */
+            public static encode(message: IS_Register, writer?: protobuf.Writer): protobuf.Writer;
+
+            /**
+             * Encodes the specified S_Register message, length delimited. Does not implicitly {@link S_Register.verify|verify} messages.
+             * @param message S_Register message or plain object to encode
+             * @param [writer] Writer to encode to
+             * @returns Writer
+             */
+            public static encodeDelimited(message: IS_Register, writer?: protobuf.Writer): protobuf.Writer;
+
+            /**
+             * Decodes a S_Register message from the specified reader or buffer.
+             * @param reader Reader or buffer to decode from
+             * @param [length] Message length if known beforehand
+             * @returns S_Register
+             * @throws {Error} If the payload is not a reader or valid buffer
+             * @throws {protobuf.util.ProtocolError} If required fields are missing
+             */
+            public static decode(reader: (protobuf.Reader | Uint8Array), length?: number): S_Register;
+
+            /**
+             * Decodes a S_Register message from the specified reader or buffer, length delimited.
+             * @param reader Reader or buffer to decode from
+             * @returns S_Register
+             * @throws {Error} If the payload is not a reader or valid buffer
+             * @throws {protobuf.util.ProtocolError} If required fields are missing
+             */
+            public static decodeDelimited(reader: (protobuf.Reader | Uint8Array)): S_Register;
+
+            /**
+             * Verifies a S_Register message.
+             * @param message Plain object to verify
+             * @returns `null` if valid, otherwise the reason why it is not
+             */
+            public static verify(message: { [k: string]: any }): (string | null);
+
+            /**
+             * Creates a S_Register message from a plain object. Also converts values to their respective internal types.
+             * @param object Plain object
+             * @returns S_Register
+             */
+            public static fromObject(object: { [k: string]: any }): S_Register;
+
+            /**
+             * Creates a plain object from a S_Register message. Also converts values to other types if specified.
+             * @param message S_Register
+             * @param [options] Conversion options
+             * @returns Plain object
+             */
+            public static toObject(message: S_Register, options?: protobuf.IConversionOptions): { [k: string]: any };
+
+            /**
+             * Converts this S_Register to JSON.
+             * @returns JSON object
+             */
+            public toJSON(): { [k: string]: any };
+        }
+
+        export declare namespace S_Register {
+
             /** Status enum. */
-            export const enum Status {
+            enum Status {
                 SUCCEED = 0,
                 ACCOUNT_INVALID = 1,
                 ACCOUNT_USED = 2,
