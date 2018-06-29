9232b38c3922ddcc8a15d659e21aeb1e8fe61954
Babygogogo <101599838@qq.com>
2018-06-29 23:22:04 +0800

    [add] Add Notify.ts.

---
+++ src/utility/Notify.ts
@@ -0,0 +1,28 @@
+
+namespace Utility {
+    export namespace Notify {
+        const dispatcher = new egret.EventDispatcher();
+
+        export type Listener = {
+            name       : string,
+            callback   : Function,
+            thisObject?: any,
+        };
+
+        export function dispatch(name: string, data?: any): void {
+            dispatcher.dispatchEventWith(name, false, data);
+        }
+
+        export function addEventListeners(listeners: Listener[], thisObject?: any): void {
+            for (const l of listeners) {
+                dispatcher.addEventListener(l.name, l.callback, l.thisObject || thisObject);
+            }
+        }
+
+        export function removeEventListeners(listeners: Listener[], thisObject?: any): void {
+            for (const l of listeners) {
+                dispatcher.removeEventListener(l.name, l.callback, l.thisObject || thisObject);
+            }
+        }
+    }
+}
