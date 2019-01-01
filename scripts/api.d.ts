1060a721b26077375a76a180429f6d48ad3c1f25
babygogogo <101599838@qq.com>
2019-01-02 00:13:08 +0800

    [update] Upgrade the engine version.

--- scripts/api.d.ts
+++ scripts/api.d.ts
@@ -250,14 +250,14 @@ declare module 'built-in' {
 
     /**
      * 发布策略
-     * * default : 使用 egretProperties.json 中的 exmlPublishPolicy 中的策略
      * * debug : 默认策略，用于开发环境
      * * contents : 将 EXML 的内容写入到主题文件中
      * * gjs : 将生成的JS文件写入到主题文件中
      * * commonjs : 将EXML合并为一个 CommonJS 风格的文件
      * * commonjs2 : 将EXML合并为一个含有解析方法和皮肤定义的文件，且皮肤抽离为一份配置
+     * * json : 将每个EXML文件生成一份配置
      */
-    type EXML_Publish_Policy = "default" | "debug" | "contents" | "gjs" | "commonjs" | "commonjs2"
+    type EXML_Publish_Policy = "debug" | "contents" | "gjs" | "commonjs" | "commonjs2" | "json"
 
 
 
@@ -315,9 +315,9 @@ declare module 'built-in' {
 
         resourceConfigFiles: { filename: string, root: string }[];
 
-        nameSelector: (url: string) => string
+        nameSelector: (url: string) => string;
 
-
+        TM_Verbose: boolean;
     }
 
     export class ConvertResConfigFilePlugin implements plugins.Command {
@@ -335,12 +335,16 @@ declare module 'built-in' {
 
     }
 
+    type TextureMergerOptions = {
+        textureMergerRoot: string[];
+    }
+
     /**
      * 使用 TextureMerger 实现纹理自动合并，依赖 TextureMerger 1.7 以上的版本
      */
     export class TextureMergerPlugin implements plugins.Command {
 
-        constructor();
+        constructor(options: TextureMergerOptions);
 
     }
 
@@ -412,4 +416,14 @@ declare module 'built-in' {
         constructor(option: ZipPluginOptions);
     }
 
+    type MergeEuiJsonPluginOptions = {
+
+        mergeSelector?: (p: string) => string | null,
+
+        createConfig?: boolean
+    }
+    export class MergeEuiJsonPlugin implements plugins.Command {
+
+        constructor(option?: MergeEuiJsonPluginOptions);
+    }
 }
